4f0ef71164b6bd3f60f018324d38dd1e490fa262
Kevin Lanvin <info.lanvink@gmail.com>
2016-01-25 18:29:18 +0100

    Commandes echo et ack fonctionnelles

--- chat_selectors/Entry.java
+++ chat_selectors/Entry.java
@@ -1,36 +1,30 @@
-package selectors;
-
-import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 public class Entry {
 	public SocketChannel channel;
 	public int initialnb;
 	public int remaining;
-	protected ByteArrayOutputStream output;
-	
-	public Entry (SocketChannel channel, int remaining){
+	public ByteBuffer output;
+
+	public Entry(SocketChannel channel, int remaining) {
 		this.initialnb = remaining;
 		this.remaining = remaining;
 		this.channel = channel;
-		this.output = new ByteArrayOutputStream();
-	}
-	
-	public Entry (SocketChannel channel){
-		this.channel = channel;
-	}
-	
-	void decrRemaining(int nb){
-		this.remaining-=nb;
+		this.output = ByteBuffer.allocate(Server.BUFFERSIZE);
 	}
 
+	public Entry(SocketChannel channel) {
+		this.channel = channel;
+	}
+
+	void decrRemaining(int nb) {
+		this.remaining -= nb;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
-		return ((Entry)obj).channel.equals(channel);
-			
-	}
-	
-	
+		return ((Entry) obj).channel.equals(channel);
 
+	}
 }
